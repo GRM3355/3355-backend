46665d4cfebd67258d36e8ca276ca274fa5c37e3
박다희 <jwp9622@naver.com>
2025-11-05 13:13:45 +0900

    [#3] chatRoomUser entity edit (#4)

--- common-lib/src/main/java/com/grm3355/zonie/commonlib/domain/chatroom/entity/ChatRoomUser.java
+++ common-lib/src/main/java/com/grm3355/zonie/commonlib/domain/chatroom/entity/ChatRoomUser.java
@@ -1,5 +1,7 @@
 package com.grm3355.zonie.commonlib.domain.chatroom.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 import com.grm3355.zonie.commonlib.domain.user.entity.User;
 import com.grm3355.zonie.commonlib.global.entity.BaseTimeEntity;
@@ -19,11 +22,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "chat_room_user")
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
+@Table(
+	name = "chat_room_user",
+	uniqueConstraints = {
+		@UniqueConstraint(
+			columnNames = {"chat_room_id", "nickname"}
+		)
+	}
+)
 public class ChatRoomUser extends BaseTimeEntity {
 
 	@Id
@@ -41,5 +51,9 @@ public class ChatRoomUser extends BaseTimeEntity {
 
 	@Column(name = "nick_name", nullable = false, length = 50)
 	private String nickName;
+
+	// 사용자가 마지막으로 읽은 시각
+	@Column(name = "last_read_at", nullable = false)
+	private LocalDateTime lastReadAt;
 
 }
