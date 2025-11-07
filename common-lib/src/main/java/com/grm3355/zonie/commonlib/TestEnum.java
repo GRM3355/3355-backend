3bf64c5a71e44795edfb25ac563cb991f31b3262
박다희 <jwp9622@naver.com>
2025-11-07 17:57:30 +0900

    [#24] TestEnum edit

--- common-lib/src/main/java/com/grm3355/zonie/commonlib/TestEnum.java
+++ common-lib/src/main/java/com/grm3355/zonie/commonlib/TestEnum.java
@@ -18,17 +18,4 @@ public enum TestEnum {
 	private String message;
 
 
-	// ✅ 생성자 추가
-	TestEnum(String code, String message) {
-		this.code = code;
-		this.message = message;
-	}
-
-	public String getCode() {
-		return code;
-	}
-
-	public String getMessage() {
-		return message;
-	}
 }
