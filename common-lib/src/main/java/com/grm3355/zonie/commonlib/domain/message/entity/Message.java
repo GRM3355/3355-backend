0bb8703709027f84b2e3450949dbeb1ee66b2768
meraki6512 <jweun123@gmail.com>
2025-11-12 06:53:59 +0900

    chore: mongodb 인덱스 추가
    일단, 채팅 내역 조회를 위한 채팅방 (id, created_at) 인덱스 생성
    
    Ref: #57
    Resolved: #62

--- common-lib/src/main/java/com/grm3355/zonie/commonlib/domain/message/entity/Message.java
+++ common-lib/src/main/java/com/grm3355/zonie/commonlib/domain/message/entity/Message.java
@@ -1,6 +1,8 @@
 package com.grm3355.zonie.commonlib.domain.message.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AccessLevel;
@@ -21,6 +23,15 @@ import com.grm3355.zonie.commonlib.domain.message.enums.MessageType;
 @NoArgsConstructor // Jackson이 객체 생성 시 필요
 @AllArgsConstructor(access = AccessLevel.PRIVATE) // Builder가 사용
 @Document(collection = "messages") 	// MongoDB 컬렉션 이름
+@CompoundIndexes({
+	// 1. 채팅 내역 조회 (과거 메시지 로드)
+	@CompoundIndex(name = "idx_chatroom_created", def = "{'chatRoomId': 1, 'createdAt': -1}"),
+	// 2~3번은 현재 기획상 불필요함 (우선 주석 처리)
+	// 2. 이미지 모아보기
+	// @CompoundIndex(name = "idx_chatroom_type_created", def = "{'chatRoomId': 1, 'type': 1, 'createdAt': -1}"),
+	// 3. 핫 이미지 조회
+	// @CompoundIndex(name = "idx_chatroom_type_like", def = "{'chatRoomId': 1, 'type': 1, 'likeCount': -1}")
+})
 public class Message {
 	@Id
 	private String id;			// Mongo의 ObjectId
