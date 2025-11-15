072c68f252826db16b55fab1cd99a3d796cf7029
박다희 <jwp9622@naver.com>
2025-11-15 11:45:40 +0900

    [#105] kakao id edit

--- api-server/src/main/java/com/grm3355/zonie/apiserver/domain/auth/domain/UserInfo.java
+++ api-server/src/main/java/com/grm3355/zonie/apiserver/domain/auth/domain/UserInfo.java
@@ -6,22 +6,25 @@ import com.grm3355.zonie.commonlib.global.enums.ProviderType;
 import com.grm3355.zonie.commonlib.global.enums.Role;
 
 public record UserInfo(
-        String socialId,
-        ProviderType providerType,
-		String email,
-        String nickname,
-        String profileImage) {
+	String socialId,
+	ProviderType providerType,
+	String email,
+	String nickname,
+	String profileImage) {
 
-    public User toUser() {
-        return User.builder()
-                .userId(socialId + "kakao")
-                .password("null")
-                .role(Role.USER)
-                .profileNickName(nickname)
-                .accountEmail(email)
-                .profileImage(profileImage)
-                .providerType(providerType)
-                .socialId(socialId)
-                .build();
-    }
+	public User toUser() {
+		// 현재시간(밀리초) + 4자리 랜덤숫자
+		String uniqueSuffix = System.currentTimeMillis() + "" + (int)(Math.random() * 9000 + 1000);
+
+		return User.builder()
+			.userId(socialId + "kakao"+ uniqueSuffix)
+			.password("null")
+			.role(Role.USER)
+			.profileNickName(nickname)
+			.accountEmail(email)
+			.profileImage(profileImage)
+			.providerType(providerType)
+			.socialId(socialId)
+			.build();
+	}
 }
